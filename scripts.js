dc852481f7e6f95f7e99dfac81579b50149701b9
Jhon Freiman Arias <jhonarias1322@gmail.com>
2021-02-20 08:06:19 -0500

    haciendo arreglos y css

--- scripts.js
+++ scripts.js
@@ -13,11 +13,16 @@
         let inputNumeroUno, producto;
 
         //eventos
+        // precioTecno.addEventListener('blur', mensaje);
         btnSubmit.addEventListener('click', liquidar);
         btnLimpiar.addEventListener('click', limpiar);
 
         //funciones
         function liquidar() {
+            if (precioTecno.value == 0) {
+                alert('Debes ingresar un valor mayor');
+                limpiar();
+            }
             inputNumeroUno = precioTecno.value;
             producto = (parseInt(inputNumeroUno) + parseInt(incremento)) * 1.15;
             precioRAYCO.value = '$ ' + numberFormat.format(producto);
@@ -27,6 +32,14 @@
             precioTecno.value = '';
             precioTecno.focus();
             precioRAYCO.value = '';
+        }
+
+        function mensaje() {
+            if (precioTecno.value == "") {
+                alert('Por favor, agrega un precio');
+                precioTecno.focus();
+            }
+
         }
     });
 }());
